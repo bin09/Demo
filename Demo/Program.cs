68cc27d13415e2cca8484f3ed48b0d98ae6ba9af
bin <ylgw0909@gmail.com>
2013-09-10 10:52:13 +0800

    commit

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -11,7 +11,7 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-           // List<Product> list = ComparerProduct();
+            // List<Product> list = ComparerProduct();
 
             List<Product> list2 = ComparerDistinct();
         }
@@ -27,11 +27,11 @@ namespace Demo
             x.Add(p2);
 
             List<Product> y = new List<Product>();
-            Product p3 = new Product() { Id =1, Name = "tea" };
+            Product p3 = new Product() { Id = 1, Name = "tea" };
             y.Add(p3);
 
             return comparerPro.Check(x, y);
-          //  return x.Except(y).ToList();
+            //  return x.Except(y).ToList();
         }
         static List<Product> ComparerDistinct()
         {
@@ -42,6 +42,14 @@ namespace Demo
             Product p2 = new Product() { Id = 2, Name = "tea" };
             x.Add(p1);
             x.Add(p2);
+
+            IEnumerable<Product> p = x.Where(value => { return value.Name == "tea"; });
+            foreach (var item in p)
+            {
+                Product t = item;
+            }
+            Product p3 = x.Find((ss => { return ss.Name == "tea"; })); 
+            IEnumerable<Product> p4= x.FindAll(t2 => { return t2.Name == "tea"; });
 
             return distinctPro.Check(x);
         }
